ba0d7b9136c3bb3467f748f6faaf34642cbfd9e0
gchh <skring.2008@163.com>
2017-06-03 17:30:26 +0800

    17-6-3-17h

---
+++ 1025. 反转链表.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+
+struct node{
+	char addr[6];
+	int num;
+    char next[6];
+};
+
+int main()
+{
+	int node_number,fz_node_number;
+	char frist_node_address[6];
+	scanf("%s %d %d", &frist_node_address,&node_number,&fz_node_number);
+	struct node LB[node_number];
+	int i;
+	struct node rs[node_number];
+	strcpy(rs[0].addr,frist_node_address);
+	for(i=0;i<node_number;i++)
+	{
+		scanf("%s %d %s", &LB[i].addr,&LB[i].num,&LB[i].next);
+		if(strcmp(LB[i].addr,rs[0].addr)==0)
+		{
+			rs[0].num=LB[i].num;
+			strcpy(rs[0].next,LB[i].next);
+		}		
+	} 
+
+	int j=1;
+	while(j<node_number)
+	{
+		for(i=0;i<node_number;i++)
+		{
+			if(strcmp(rs[j-1].next,LB[i].addr)==0)//(rs[j].next==LB[i].addr)
+			{
+				strcpy(rs[j].addr,LB[i].addr);
+				rs[j].num=LB[i].num;
+				strcpy(rs[j].next,LB[i].next);
+				j++;
+				break;
+			}
+		}
+	}
+	
+
+#if 0
+	struct node fz[node_number];
+	j=0;
+	int k,t;
+	for(k=0;k<node_number/fz_node_number;k++)
+	{
+		t=k*fz_node_number;
+		for(i=t+fz_node_number-1;i>=t;i--)
+		{
+			strcpy(fz[j].addr,rs[i].addr);
+			fz[j].num=rs[i].num;
+			if(i==t)strcpy(fz[j].next,rs[t+fz_node_number-1].next);
+			else strcpy(fz[j].next,rs[i-1].addr);
+			j++;
+		}
+	}
+	for(i=j;i<node_number;i++)
+	{
+		strcpy(fz[i].addr,rs[i].addr);
+		fz[i].num=rs[i].num;
+		strcpy(fz[i].next,rs[i].next);
+	}
+#else
+	struct node fz[node_number];
+	j=0;
+	int k,t;
+	for(k=node_number/fz_node_number-1;k>=0;k--)
+	{
+		t=k*fz_node_number;
+		for(i=t+fz_node_number-1;i>=t;i--)
+		{
+			strcpy(fz[i].addr,rs[i-fz_node_number+1].addr);
+			fz[i].num=rs[i-fz_node_number+1].num;
+			//if(i==t)strcpy(fz[j].next,rs[t+fz_node_number-1].next);
+			//else strcpy(fz[j].next,rs[i-1].addr);
+			strcpy(fz[i].next,rs[i+1].addr);
+			j++;
+		}
+	}
+	for(i=node_number-node_number%fz_node_number;i<node_number;i++)
+	{
+		strcpy(fz[i].addr,rs[i].addr);
+		fz[i].num=rs[i].num;
+		strcpy(fz[i].next,rs[i].next);
+	}
+#endif
+/*
+	for(i=0;i<node_number-1;i++)
+	{
+		if(strcmp(fz[i].next,fz[i+1].addr)!=0)strcpy(fz[i].next,fz[i+1].addr);
+	} 
+*/
+	for(i=0;i<node_number;i++)
+	{
+		printf("%s %d %s\n", fz[i].addr,fz[i].num,fz[i].next);
+	} 
+	return 0;
+} 
