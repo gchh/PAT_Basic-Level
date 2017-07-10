f1e0ddcee2ccc603b13ed59f9e4f668fa67de9cf
gchh <skring.2008@163.com>
2017-07-10 15:22:10 +0800

    17-7-10-15h

--- 1069. 微博转发抽奖.c
+++ 1069. 微博转发抽奖.c
@@ -19,9 +19,11 @@ int main()
 	//struct p nm[1001];
 	//for(i=1;i<=m;i++)nm[i].t=0;//未中奖 
 	struct p *nm=(struct p*)malloc(sizeof(struct p)*(m+1));	
+	int flag=0;
 	for(i=1;i<=m;i++)
 	{
 		scanf("%s",&nm[i].name);
+#if 0//测试点3，答案错误 
 		if((cnt-s)%n==0&&n>0)
 		{
 			nm[i].t=1;//中奖
@@ -36,12 +38,34 @@ int main()
 			}
 		}
 		if(j==i)cnt++;
+#else//全部正确 
+		if(cnt==s&&flag==0||cnt==n&&flag==1)
+		{
+			for(j=1;j<i;j++)
+			{
+				if(strcmp(nm[i].name,nm[j].name)==0&&nm[j].t==1)
+				{
+					cnt--;//重复中奖，跳过此人 
+					break;
+				}
+			}
+			if(j==i)
+			{
+				if(cnt==s&&flag==0)flag=1;
+				nm[i].t=1;//中奖
+				cnt=1;
+			}
+		}		
+		if(nm[i].t==0)
+		{
+			cnt++;
+		}
+#endif
 	}
 	for(i=s;i<=m;i++)
-	{
+	{ 
 		if(nm[i].t==1)puts(nm[i].name);
 	}
 	free(nm);
 	return 0;
 }
-//测试点3，答案错误 
